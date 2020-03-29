9a01b95a001a86a7d62126f923f938044dbb1b0b
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2020-03-29 18:44:41 +1100

    Agrega fixture y pruebas unitarias

--- src/__tests__/pokedex.spec.js
+++ src/__tests__/pokedex.spec.js
@@ -1,7 +1,27 @@
 import inicializar from '../pokedex.js';
+import fixture from './pokedex.fixture.js';
+import listadoFixture from '../../cypress/fixtures/listado-pagina-1.json';
 
-describe('index', () => {
-  it('inicializa pokedex', () => {
-    console.log(inicializar);
-  });
+
+test('inicializa pokedex', () => {
+  document.body.innerHTML = fixture;
+  global.fetch = jest.fn()
+    .mockImplementation(() => new Promise((resolve) => {
+      const jsonPromise = new Promise((r) => {
+        r(listadoFixture);
+      });
+      resolve({ json: () => jsonPromise });
+    }));
+
+  inicializar()
+    .then(() => {
+      expect(document.querySelector('#ayuda').textContent)
+        .toContain('Seleccioná un pokemon para ver su información');
+
+      expect(document.querySelector('#total-pokemones').textContent)
+        .toContain('964');
+
+      expect(document.querySelectorAll('#indice .list-group-item'))
+        .toHaveLength(20);
+    });
 });
