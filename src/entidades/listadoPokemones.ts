7e1b4a4a4dd5c1d72bf86609aeb0702ce5fbf2ff
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2021-05-02 08:25:22 +1000

    Agrega integración con typescript

---
+++ src/entidades/listadoPokemones.ts
@@ -0,0 +1,4 @@
+export default class ListadoPokemones {
+  constructor(private total: number, private siguienteUrl: string, private anteriorUrl: string, private nombresPokemones: string[]) {
+  }
+}
