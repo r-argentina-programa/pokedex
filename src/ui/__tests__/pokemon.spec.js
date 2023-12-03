a112d2471ccd2e60ef235c5e8147a57189295fd5
Gruvix <timmy.burton@hotmail.com>
2023-12-03 12:09:13 -0300

    implementacion inicial pruebas de mostrarPokemon
    Preparación de las pruebas en Jest de mostrarPokemon de ui/pokemon.js
    incluye mocks de el cuerpo del html y de la variable pokemon que pide la función

--- src/ui/__tests__/pokemon.spec.js
+++ src/ui/__tests__/pokemon.spec.js
@@ -1,1 +1,24 @@
 import mostrarPokemon from '../pokemon.js';
+import fixture from './pokemon.fixture.js';
+
+test('muestra el pokemon', () => {
+  document.body.innerHTML = fixture;
+  const pokemon = {
+    id: 1,
+    nombre: 'nombre',
+    foto: 'https://example.com/test.png',
+    tipos: ['tipo1', 'tipo2'],
+    habilidades: ['habilidad1', 'habilidad2'],
+    movimientos: [
+      {
+        nombre: 'movimiento1',
+        versiones: ['version1', 'version2'],
+      },
+      {
+        nombre: 'movimiento2',
+        versiones: ['version3', 'version4'],
+      },
+    ],
+  };
+  mostrarPokemon(pokemon);
+});
