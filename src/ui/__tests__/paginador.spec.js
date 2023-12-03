1cc80e81be3dcde06b508431e29c6f18e073ea3f
Gruvix <timmy.burton@hotmail.com>
2023-12-03 11:01:06 -0300

    Agrega prueba para la funcion manejarCambioPagina
    agrega pruebas en jest para la funcion  manejarCambioPagina que mockea el evento y la funcion de callback, y chekea q todo sea llamado adecuadamente

--- src/ui/__tests__/paginador.spec.js
+++ src/ui/__tests__/paginador.spec.js
@@ -1,2 +1,21 @@
 import mostrarPaginador, { manejarCambioPagina } from '../paginador';
 
+test('maneja el cambio de pagina', () => {
+  const callback = jest.fn();
+  const e = {
+    preventDefault: jest.fn(),
+    target: {
+      getAttribute: () => '#',
+      dataset: {
+        pagina: '1',
+      },
+    },
+  };
+  manejarCambioPagina(e, callback);
+  expect(e.preventDefault)
+    .toHaveBeenCalledTimes(1);
+  expect(callback)
+    .toHaveBeenCalledTimes(1);
+  expect(callback)
+    .toHaveBeenCalledWith(1);
+});
