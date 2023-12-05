7f1f23c8b43c2457258ab1c52fbfe9eb40c6fc8d
Gruvix <timmy.burton@hotmail.com>
2023-12-04 23:54:51 -0300

    mueve las pruebas adentro de un describe

--- src/ui/__tests__/listado.spec.js
+++ src/ui/__tests__/listado.spec.js
@@ -1,17 +1,19 @@
 import { actualizarTextoIndicePokemones, mostrarListadoPokemones } from '../listado.js';
 
-test('actualiza el texto de indice de pokemons', () => {
-  document.body.innerHTML = '<div id="indice"></div>';
-  actualizarTextoIndicePokemones('test');
-  expect(document.querySelector('#indice').textContent)
-    .toContain('test');
-});
-
-test('muestra el listado de pokemones', () => {
-  document.body.innerHTML = '<div id="indice"></div>';
-  mostrarListadoPokemones(['test'], jest.fn());
-  expect(document.querySelector('#indice').children.length)
-    .toBe(1);
-  expect(document.querySelector('#indice').children[0].textContent)
-    .toContain('test');
-});
+describe('pruebas de listado', () => {
+  it('actualiza el texto de indice de pokemons', () => {
+    document.body.innerHTML = '<div id="indice"></div>';
+    actualizarTextoIndicePokemones('test');
+    expect(document.querySelector('#indice').textContent)
+      .toContain('test');
+  });
+  
+  it('muestra el listado de pokemones', () => {
+    document.body.innerHTML = '<div id="indice"></div>';
+    mostrarListadoPokemones(['test'], jest.fn());
+    expect(document.querySelector('#indice').children.length)
+      .toBe(1);
+    expect(document.querySelector('#indice').children[0].textContent)
+      .toContain('test');
+  });
+  
